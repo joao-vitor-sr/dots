9369ae3da198afd68730fb8a6cbf60a44deaac4c
Joao Vitor <joaovitorsrs7@gmail.com>
2022-01-03 23:26:54 -0300

    now the plugin manager is packer.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,42 +1,6 @@
 vim.g.mapleader = ","
 
-require "paq" {
-	-- general
-	"kyazdani42/nvim-web-devicons", -- icons
-	"alvarosevilla95/luatab.nvim",
-	"unblevable/quick-scope",
-	"mhartington/formatter.nvim",
-	"numToStr/Comment.nvim",
-	"joao-vitor-sr/nvimDvorak",
-	-- lsp
-	"neovim/nvim-lspconfig",
-	"hrsh7th/cmp-vsnip",
-	"hrsh7th/vim-vsnip-integ",
-	"hrsh7th/vim-vsnip",
-	"rafamadriz/friendly-snippets",
-	"ray-x/lsp_signature.nvim",
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/nvim-cmp",
-	"hrsh7th/cmp-buffer",
-	"hrsh7th/cmp-path",
-	"hrsh7th/cmp-cmdline",
-	"onsails/lspkind-nvim", -- cmp menu
-	"tami5/lspsaga.nvim",
-	-- files
-	"kyazdani42/nvim-tree.lua",
-	"nvim-lua/plenary.nvim",
-	"nvim-telescope/telescope.nvim",
-	-- Tim pope plugins
-	"tpope/vim-sensible",
-	"tpope/vim-fugitive",
-	"tpope/vim-surround",
-	"tpope/vim-obsession",
-	"tpope/vim-repeat",
-	-- theme
-	"shaunsingh/nord.nvim",
-	"nvim-lualine/lualine.nvim",
-	{"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
-}
+require("plugins")
 
 -- lua configs
 require("general")
---
+++ nvim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,49 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require("packer").startup(
+	function()
+		-- general
+		use "wbthomason/packer.nvim"
+		use "kyazdani42/nvim-web-devicons" -- icons
+		use "alvarosevilla95/luatab.nvim"
+		use "unblevable/quick-scope"
+		use "mhartington/formatter.nvim"
+		use "numToStr/Comment.nvim"
+		use "joao-vitor-sr/nvimDvorak"
+
+		-- lsp
+		use "neovim/nvim-lspconfig"
+		use "hrsh7th/cmp-vsnip"
+		use "hrsh7th/vim-vsnip-integ"
+		use "hrsh7th/vim-vsnip"
+		use "rafamadriz/friendly-snippets"
+		use "ray-x/lsp_signature.nvim"
+		use "hrsh7th/cmp-nvim-lsp"
+		use "hrsh7th/nvim-cmp"
+		use "hrsh7th/cmp-buffer"
+		use "hrsh7th/cmp-path"
+		use "hrsh7th/cmp-cmdline"
+		use "onsails/lspkind-nvim" -- cmp menu
+		use {"tami5/lspsaga.nvim"} -- nightly
+
+		-- files
+		use "kyazdani42/nvim-tree.lua"
+		use "nvim-lua/plenary.nvim"
+		use "nvim-telescope/telescope.nvim"
+
+		-- Tim pope plugins
+		use "tpope/vim-sensible"
+		use "tpope/vim-fugitive"
+		use "tpope/vim-surround"
+		use "tpope/vim-obsession"
+		use "tpope/vim-repeat"
+
+		-- theme
+		use "shaunsingh/nord.nvim"
+		use "nvim-lualine/lualine.nvim"
+		use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+	end
+)
