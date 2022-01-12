3b9bdf27229b451cbcbe69995446ae5e2a74499e
Joao Vitor <joaovitorsrs7@gmail.com>
2022-01-12 11:14:47 -0300

    Now the lspsaga rempas were split.

--- nvim/.config/nvim/lua/lspconfig-config.lua
+++ nvim/.config/nvim/lua/lspconfig-config.lua
@@ -62,19 +62,12 @@ local on_attach = function(client, bufnr)
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
 	buf_set_keymap("n", "gD", "<cmd>Lspsaga preview_definition<CR>", opts)
 	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	buf_set_keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
 	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 	buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 	buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
 	buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
 	buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
 	buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-	buf_set_keymap("n", "<space>rn", "<cmd>Lspsaga rename<cr>", opts)
-	buf_set_keymap("n", "<space>ca", "<cmd>Lspsaga code_action<cr>", opts)
-	buf_set_keymap("n", "<space>gr", "<cmd>Lspsaga lsp_finder<cr>", opts)
-	buf_set_keymap("n", "<space>e", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
-	buf_set_keymap("n", "[d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-	buf_set_keymap("n", "]d", "<cmd>Lspsaga` diagnostic_jump_prev<CR>", opts)
 	buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
 	buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 end
@@ -88,3 +81,11 @@ for _, lsp in ipairs(servers) do
 		}
 	}
 end
+
+vim.api.nvim_set_keymap("n", "<space>e", "<cmd>Lspsaga show_line_diagnostics<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<space>rn", "<cmd>Lspsaga rename<cr>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<space>ca", "<cmd>Lspsaga code_action<cr>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<space>gr", "<cmd>Lspsaga lsp_finder<cr>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "[d", "<cmd>Lspsaga diagnostic_jump_next<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "]d", "<cmd>Lspsaga` diagnostic_jump_prev<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", {noremap = true, silent = true})
