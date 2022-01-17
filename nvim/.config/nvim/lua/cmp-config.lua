4d9b3d52b5a700bdf5b936d4ac1ffabe840c1ca2
Joao Vitor <joaovitorsrs7@gmail.com>
2022-01-17 18:38:44 -0300

    New cmp rempas for the nvim.

--- nvim/.config/nvim/lua/cmp-config.lua
+++ nvim/.config/nvim/lua/cmp-config.lua
@@ -7,15 +7,6 @@ local cmp_buffer = require("cmp_buffer")
 local lspkind = require "lspkind"
 lspkind.init()
 
-local feedkey = function(key, mode)
-	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
-
-local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 cmp.setup(
 	{
 		completion = {
@@ -56,48 +47,30 @@ cmp.setup(
 					end
 				end
 			},
-			["<C-b>"] = cmp.mapping {
-				c = function()
-					if cmp.visible() then
-						cmp.select_prev_item {behavior = cmp.SelectBehavior.Insert}
-					end
-				end,
-				i = function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item {behavior = cmp.SelectBehavior.Insert}
-					else
-						fallback()
-					end
-				end
-			},
 			["<C-Period>"] = cmp.mapping.close(),
 			["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), {"i", "c"}),
-			["<Tab>"] = cmp.mapping(
+			["<tab>"] = cmp.config.disable,
+			["<C-b>"] = cmp.mapping.confirm({select = true}),
+			["<C-d>"] = cmp.mapping(
 				function(fallback)
 					if cmp.visible() then
 						cmp.select_next_item()
-					elseif vim.fn["vsnip#available"](1) == 1 then
-						feedkey("<Plug>(vsnip-expand-or-jump)", "")
-					elseif has_words_before() then
-						cmp.complete()
 					else
 						fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
 					end
 				end,
 				{"i", "s"}
 			),
-			["<S-Tab>"] = cmp.mapping(
-				function()
+			["<C-i>"] = cmp.mapping(
+				function(fallback)
 					if cmp.visible() then
 						cmp.select_prev_item()
-					elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-						feedkey("<Plug>(vsnip-jump-prev)", "")
+					else
+						fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
 					end
 				end,
 				{"i", "s"}
-			),
-			-- ... Your other mappings ...
-			["<CR>"] = cmp.mapping.confirm({select = true})
+			)
 		},
 		-- REQUIRED - you must specify a snippet engine
 		snippet = {
@@ -107,7 +80,7 @@ cmp.setup(
 		},
 		sources = {
 			{name = "nvim_lsp"},
-			{name = "vsnip"}, -- For vsnip users.
+			{name = "vsnip"} -- For vsnip users.
 			-- {name = "buffer", keyword_length = 5}
 		},
 		sorting = {
