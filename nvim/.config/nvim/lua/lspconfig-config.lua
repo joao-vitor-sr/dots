0e30d782eec9846bd162afcd00b4bea4ee50c2e9
Joao Vitor <joaovitorsrs7@gmail.com>
2022-03-31 15:42:09 -0300

    New nvim formatter for lua, now the formatter is stylua no more luafmt.

--- nvim/.config/nvim/lua/lspconfig-config.lua
+++ nvim/.config/nvim/lua/lspconfig-config.lua
@@ -2,85 +2,85 @@ local nvim_lsp = require("lspconfig")
 
 local lspsaga = require "lspsaga"
 lspsaga.setup {
-	-- defaults ...
-	debug = false,
-	use_saga_diagnostic_sign = true,
-	-- diagnostic sign
-	error_sign = "",
-	warn_sign = "",
-	hint_sign = "",
-	infor_sign = "",
-	-- code action title icon
-	code_action_icon = " ",
-	code_action_prompt = {
-		enable = true,
-		sign = true,
-		sign_priority = 40,
-		virtual_text = true
-	},
-	finder_definition_icon = "  ",
-	finder_reference_icon = "  ",
-	max_preview_lines = 10,
-	finder_action_keys = {
-		open = "o",
-		vsplit = "s",
-		split = "i",
-		quit = "q",
-		scroll_down = "<C-f>",
-		scroll_up = "<C-b>"
-	},
-	code_action_keys = {
-		quit = "q",
-		exec = "<CR>"
-	},
-	rename_action_keys = {
-		quit = "<C-c>",
-		exec = "<CR>"
-	},
-	definition_preview_icon = "  ",
-	border_style = "single",
-	rename_prompt_prefix = "➤",
-	server_filetype_map = {},
-	diagnostic_prefix_format = "%d. "
+    -- defaults ...
+    debug = false,
+    use_saga_diagnostic_sign = true,
+    -- diagnostic sign
+    error_sign = "",
+    warn_sign = "",
+    hint_sign = "",
+    infor_sign = "",
+    -- code action title icon
+    code_action_icon = " ",
+    code_action_prompt = {
+        enable = true,
+        sign = true,
+        sign_priority = 40,
+        virtual_text = true
+    },
+    finder_definition_icon = "  ",
+    finder_reference_icon = "  ",
+    max_preview_lines = 10,
+    finder_action_keys = {
+        open = "o",
+        vsplit = "s",
+        split = "i",
+        quit = "q",
+        scroll_down = "<C-f>",
+        scroll_up = "<C-b>"
+    },
+    code_action_keys = {
+        quit = "q",
+        exec = "<CR>"
+    },
+    rename_action_keys = {
+        quit = "<C-c>",
+        exec = "<CR>"
+    },
+    definition_preview_icon = "  ",
+    border_style = "single",
+    rename_prompt_prefix = "➤",
+    server_filetype_map = {},
+    diagnostic_prefix_format = "%d. "
 }
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-	local function buf_set_keymap(...)
-		vim.api.nvim_buf_set_keymap(bufnr, ...)
-	end
-	local function buf_set_option(...)
-		vim.api.nvim_buf_set_option(bufnr, ...)
-	end
+    local function buf_set_keymap(...)
+        vim.api.nvim_buf_set_keymap(bufnr, ...)
+    end
+    local function buf_set_option(...)
+        vim.api.nvim_buf_set_option(bufnr, ...)
+    end
 
-	-- Enable completion triggered by <c-x><c-o>
-	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+    -- Enable completion triggered by <c-x><c-o>
+    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-	-- Mappings.
-	local opts = {noremap = true, silent = true}
+    -- Mappings.
+    local opts = {noremap = true, silent = true}
 
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	buf_set_keymap("n", "gD", "<cmd>Lspsaga preview_definition<CR>", opts)
-	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-	buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-	buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-	buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-	buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-	buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-	buf_set_keymap("n", "<space>k", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    buf_set_keymap("n", "gD", "<cmd>Lspsaga preview_definition<CR>", opts)
+    buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+    buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+    buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+    buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+    buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    buf_set_keymap("n", "<space>k", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
 end
 
-local servers = {"tsserver", "eslint", "clangd", "intelephense", "hls", "gopls", "pyright"}
+local servers = {"tsserver", "eslint", "clangd", "intelephense", "hls", "pyright", "rust_analyzer"}
 for _, lsp in ipairs(servers) do
-	nvim_lsp[lsp].setup {
-		on_attach = on_attach,
-		flags = {
-			debounce_text_changes = 150
-		}
-	}
+    nvim_lsp[lsp].setup {
+        on_attach = on_attach,
+        flags = {
+            debounce_text_changes = 150
+        }
+    }
 end
 
 vim.api.nvim_set_keymap("n", "<space>e", "<cmd>Lspsaga show_line_diagnostics<CR>", {noremap = true, silent = true})
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,56 +1,54 @@
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require("packer").startup(
-    function(use)
-        use "wbthomason/packer.nvim"
-        -- Tim pope plugins
-        use "tpope/vim-sensible"
-        use "tpope/vim-surround"
-        use "tpope/vim-obsession"
-        use "tpope/vim-repeat"
+return require('packer').startup(function(use)
+	use('wbthomason/packer.nvim')
+	-- Tim pope plugins
+	use('tpope/vim-sensible')
+	use('tpope/vim-surround')
+	use('tpope/vim-obsession')
+	use('tpope/vim-repeat')
 
-        -- general
-        use "kyazdani42/nvim-web-devicons" -- icons
-        use {
-            "alvarosevilla95/luatab.nvim",
-            config = function()
-                require("luatab").setup {}
-            end
-        }
-        use "unblevable/quick-scope"
-        use "mhartington/formatter.nvim"
-        use "numToStr/Comment.nvim"
-        use "joao-vitor-sr/nvimDvorak"
+	-- general
+	use('kyazdani42/nvim-web-devicons') -- icons
+	use({
+		'alvarosevilla95/luatab.nvim',
+		config = function()
+			require('luatab').setup({})
+		end,
+	})
+	use('unblevable/quick-scope')
+	use('mhartington/formatter.nvim')
+	use('numToStr/Comment.nvim')
+	use('joao-vitor-sr/nvimDvorak')
 
-        -- lsp
-        use "neovim/nvim-lspconfig"
-        use "rafamadriz/friendly-snippets"
-        use "ray-x/lsp_signature.nvim"
-        use "hrsh7th/cmp-nvim-lsp"
+	-- lsp
+	use('neovim/nvim-lspconfig')
+	use('rafamadriz/friendly-snippets')
+	use('ray-x/lsp_signature.nvim')
+	use('hrsh7th/cmp-nvim-lsp')
 
-        -- snip
-        use "L3MON4D3/LuaSnip"
-        use "saadparwaiz1/cmp_luasnip"
+	-- snip
+	use('L3MON4D3/LuaSnip')
+	use('saadparwaiz1/cmp_luasnip')
 
-        -- use "~/codes/lua/nvim-cmp"
-        use "hrsh7th/nvim-cmp"
-        use "hrsh7th/cmp-buffer"
-        use "hrsh7th/cmp-path"
-        use "hrsh7th/cmp-cmdline"
-        use "onsails/lspkind-nvim" -- cmp menu
-        use "tami5/lspsaga.nvim" -- nightly
+	-- use "~/codes/lua/nvim-cmp"
+	use('hrsh7th/nvim-cmp')
+	use('hrsh7th/cmp-buffer')
+	use('hrsh7th/cmp-path')
+	use('hrsh7th/cmp-cmdline')
+	use('onsails/lspkind-nvim') -- cmp menu
+	use('tami5/lspsaga.nvim') -- nightly
 
-        -- files
-        use "kyazdani42/nvim-tree.lua"
-        use "nvim-lua/plenary.nvim"
-        use "nvim-telescope/telescope.nvim"
+	-- files
+	use('kyazdani42/nvim-tree.lua')
+	use('nvim-lua/plenary.nvim')
+	use('nvim-telescope/telescope.nvim')
 
-        -- theme
-        use "shaunsingh/nord.nvim"
-        use "ghifarit53/tokyonight-vim"
-        use "nvim-lualine/lualine.nvim"
-        use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
-        use "norcalli/nvim-colorizer.lua"
-        use "p00f/nvim-ts-rainbow"
-    end
-)
+	-- theme
+	use('shaunsingh/nord.nvim')
+	use('ghifarit53/tokyonight-vim')
+	use('nvim-lualine/lualine.nvim')
+	use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
+	use('norcalli/nvim-colorizer.lua')
+	use('p00f/nvim-ts-rainbow')
+end)
