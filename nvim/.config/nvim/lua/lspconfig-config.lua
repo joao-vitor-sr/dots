e7f7dba2352651fb2db5bc698e026c96fd3a9b79
Joao Vitor <joaovitorsrs7@gmail.com>
2022-05-05 23:11:14 -0300

    New lsp-config for nvim.

--- nvim/.config/nvim/lua/lspconfig-config.lua
+++ nvim/.config/nvim/lua/lspconfig-config.lua
@@ -76,8 +76,9 @@ end
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-local servers = { 'tsserver', 'eslint', 'clangd', 'intelephense', 'hls', 'pyright', 'rust_analyzer', 'bashls', 'sumneko_lua' }
+local servers = { 'tsserver', 'eslint', 'clangd', 'intelephense', 'hls', 'pyright', 'rust_analyzer', 'bashls', 'sumneko_lua', 'jsonls' }
   for _, lsp in ipairs(servers) do
 	nvim_lsp[lsp].setup({
 		on_attach = on_attach,
